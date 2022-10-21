afdbdd0a9efbbdb997c086e827ac838b726c2457
Jeremy Burks <97761340+JeremyGBurks@users.noreply.github.com>
2022-10-21 00:12:00 -0700

    Update Ops_Chall32.py
    Updated, functions as it should

--- Ops_Chall32.py
+++ Ops_Chall32.py
@@ -1,61 +1,42 @@
 #!/usr/bin/env python3
 
 # Author - Jeremy Burks
-# Date Last Revised - 10/17/22
+# Date Last Revised - 10/20/22
 # Purpose - User selects an OS, then recursively scans each file and folder in the user input directory path and print it to the screen.
-#Generate the file’s MD5 hash using Hashlib, assign the MD5 hash to a variable. Print the variable to the screen along with a timestamp, file name, file size, and complete file path
+# Generate the file’s MD5 hash using Hashlib, assign the MD5 hash to a variable. Print the variable to the screen along with a timestamp, file name, file size, and complete file path
 
 import os
 import os.path
 import hashlib 
+import datetime
 
-def usermenu_os():
-    method_menu = """
-    **********************************
-    **                              **
-    *     Select an OS               *
-    *    1. Windows                  *
-    *    2. Linux                    *
-    **                              **
-    **********************************
-    """
-    os_type = input(method_menu) 
-    # upon selection of option 1 or 2, the next output prompts the user for a filename, then a directory 
-    user_file = input("please enter a file name that you'd like to find: ")
+def usermenu():
     user_dir = input("Please enter a directory path to search in: ")
-    if os_type == '1':
-        windows_select(user_dir, user_file)
-    elif os_type == '2':
-        linux_select(user_dir, user_file)
+    if os.path.exists(user_dir):
+        directory_search(user_dir)
     else:
-        print("Please choose a valid response!")
-        usermenu_os()
+        print('Please enter a valid path')
+        usermenu()
 
-def windows_select(directory, file):
-    num_files = 0
-    correct_files = 0
-    fullpath = "C:\\" + directory 
-    for filename in os.listdir(fullpath):
-        num_files += 1
-        if os.path.isdir(path):
-            print(file + " has been located in " + directory + "!")
+def directory_search(file):
+    for filename in os.listdir(file):
+        filepath = os.path.abspath(file)
+        fullpath = filepath + "/" + filename
+        if os.path.isdir(filename):
+            directory_search(fullpath)
         else:
-            num_files += 1
-    print("There were " + str(correct_files) + " matching file names found and " + str(num_files) + " were searched in this directory.")
+            hash = hashing(fullpath)
+            file_size = os.path.getsize(fullpath)
+            time = datetime.datetime.now()
+            print(hash + 'filename ' + filename, time, file_size, fullpath)
+            
 
-def linux_select(directory, file):
-    num_files = 0
-    correct_files = 0
-    for filename in os.listdir(directory):
-        num_files += 1
-        if os.path.isdir(path):
-            hash_path = path.encode('utf-8')
-            hashed_path2 = hashlib.md5(hash_path)
-            print(hashed_path2)
-            print(file + " has been located in " + directory + "!")
-        else:
-            num_files += 1
-    print("There were " + str(correct_files) + " matching file names found and " + str(num_files) + " were searched in this directory.")
+def hashing(filepath):
+    hasher = hashlib.md5()
+    with open(filepath, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
 
+usermenu()
 
-usermenu_os()
